98e72a34857ab798d5167d57ec992088cf435c08
ewagner <ericwagnerpreston@gmail.com>
2021-12-19 18:59:47 -0600

    Route new components and create links to/from home & login success pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Login from './components/Login';
-import { AuthProvider } from './contexts/AuthContext';
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import LoginSuccess from "./components/LoginSuccess";
+import Home from "./components/Home";
+import { AuthProvider } from "./contexts/AuthContext";
 
-import './App.css';
-import Signup from './components/Signup';
+import "./App.css";
+import Signup from "./components/Signup";
 
 function App() {
   return (
@@ -12,8 +14,10 @@ function App() {
       <Router>
         <AuthProvider>
           <Switch>
-            <Route exact path="/" component={Signup} />
-
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login-success" component={LoginSuccess} />
           </Switch>
         </AuthProvider>
       </Router>
